f324e6109417f0ee7db06aae8a19843bfcdbff7b
thewesker <talorb@me.com>
2023-04-10 19:27:49 -0400

    Updating for latest discord.py

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from validator import extractUrl, isSupportedUrl
 
 load_dotenv()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
